b4dda1e87da935a5c540fea0f3623c87928bede3
mrdarip <darioelas2005@gmail.com>
2024-07-05 22:49:47 +0200

    implemented getting projects info from json

--- js/index.js
+++ js/index.js
@@ -1,14 +1,36 @@
 var projectsRoutes = [
- "../projects/SampleProject/"
-]
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+  "../projects/SampleProject/",
+];
 
-for(let project of projectsRoutes){
-    let card = document.createElement('article')
-    card.className = 'card'
-    card.innerHTML = `
-        <h3>${"project.name"}</h3>
-        <p>${"project.description"}</p>
-        <a href="${"project.link"}">Link</a>
-    `
-    document.getElementById('projects').appendChild(card)
+for (let projectRoute of projectsRoutes) {
+  fetch(projectRoute + "info.json")
+    .then((res) => res.text())
+    .then((text) => {
+      project = JSON.parse(text);
+
+      let card = document.createElement("article");
+
+      card.className = "card";
+      card.innerHTML = `
+        <h3>${project["name"]}</h3>
+        <p>${project["description"]}</p>
+        <a href="${projectRoute + "index.html"}">Link</a>
+    `;
+      document.getElementById("projects").appendChild(card);
+    })
+    .catch((e) => console.error(e));
 }

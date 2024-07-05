344e55cdfdc553ec03fa33b2cc19549672b46e11
mrdarip <darioelas2005@gmail.com>
2024-07-05 12:52:25 +0200

    created folder structure and js for loading projects cards in index.html

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var projectsRoutes = [
+ "../projects/SampleProject/"
+]
+
+for(let project of projectsRoutes){
+    let card = document.createElement('div')
+    card.className = 'card'
+    card.innerHTML = `
+        <h3>${"project.name"}</h3>
+        <p>${"project.description"}</p>
+        <a href="${"project.link"}">Link</a>
+    `
+    document.getElementById('projects').appendChild(card)
+}

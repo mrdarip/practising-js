18b7477df9525a80403e17a2654a88893cdec228
mrdarip <darioelas2005@gmail.com>
2024-07-07 00:46:34 +0200

    implemented main mole logic

--- projects/Whack-A-Mole/game.js
+++ projects/Whack-A-Mole/game.js
@@ -1,6 +1,37 @@
 const molesEmojis = ["🐀", "🦡", "🐁"];
 const nonMoleEmojis = ["🕳", "🥔"];
+const allEmojis = [...molesEmojis, ...nonMoleEmojis];
 
-var gameLoop = setInterval(() => {
-    console.log("Game loop running");
-  }, 1000);
+const gameBoard = document.getElementsByTagName("main")[0];
+
+insertMole();
+insertMole();
+insertMole();
+
+function insertMole() {
+  const mole = document.createElement("p");
+  mole.innerText = allEmojis[Math.floor(Math.random() * allEmojis.length)];
+  mole.classList.add("mole");
+
+  mole.style.left = `${ Math.random() * 100}%`;
+  mole.style.top = `${ Math.random() * 100}%`;
+
+  gameBoard.appendChild(mole);
+
+  mole.addEventListener("click", () => {
+    if (molesEmojis.includes(mole.innerText)) {
+      mole.remove();
+      insertMole();
+    } else {
+      alert("Game Over");
+      location.reload();
+    }
+  });
+
+  setTimeout(() => {
+    if (mole.parentNode === gameBoard) {
+      mole.remove();
+      insertMole();
+    }
+  }, 2000 + Math.random() * 2000);
+}

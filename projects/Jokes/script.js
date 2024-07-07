9109c2fd21d83a0a59397e2463a91c23ffe30226
mrdarip <darioelas2005@gmail.com>
2024-07-07 17:33:40 +0200

    implemented jokes script

---
+++ projects/Jokes/script.js
@@ -0,0 +1,29 @@
+const setup = document.getElementById('setup');
+const skip = document.getElementById('skip');
+const show = document.getElementById('show');
+const delivery = document.getElementById('delivery');
+
+const skipTexts = ["skip", "next", "another one", "more", "refresh", "new", "next joke", "another joke", "more jokes", "refresh joke", "new joke", "haha, next one", "that was good, next one", "that was bad, next one", "that was funny, next one", "that was not funny, next one", "that was hilarious, next one", "that was boring, next one", "that was interesting, next one", "that was stupid, next one", "that was dumb, next one", "that was smart, next one", "that was clever, next one", "that was lame, next one", "that was cool, next one", "that was awesome"];
+const showTexts = ["show me", "show", "reveal", "display", "tell me", "tell", "show punchline", "show delivery", "show me punchline", "show me delivery", "show me the punchline", "show me the delivery", "show punchline now", "show delivery now", "show me punchline now", "idk, show me", "idk, show", "idk, reveal", "idk, display", "idk, tell me", "idk, tell", "idk, show punchline", "idk, show delivery", "idk, show me punchline", "idk, show me delivery", "idk, show me the punchline", "idk, show me the delivery", "idk, show punchline now", "idk, show delivery now", "idk, show me punchline now", "idk, show me delivery now", "show me the punchline", "show me the delivery"]
+
+refreshJoke();
+
+skip.addEventListener('click', refreshJoke);
+
+show.addEventListener('click', () => {
+    delivery.style.display = 'block';
+});
+
+function refreshJoke() {
+    fetch('https://v2.jokeapi.dev/joke/Any?type=twopart')
+        .then(response => response.json())
+        .then(data => {
+            skip.innerHTML = skipTexts[Math.floor(Math.random() * skipTexts.length)];
+            show.innerHTML = showTexts[Math.floor(Math.random() * showTexts.length)];
+            
+            delivery.style.display = 'none';
+
+            setup.innerHTML = data.setup;
+            delivery.innerHTML = data.delivery;
+        });
+}

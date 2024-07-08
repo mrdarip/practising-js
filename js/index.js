bb3e27c0e88910d89aff2e61b8a688400883d1c1
mrdarip <darioelas2005@gmail.com>
2024-07-08 14:30:31 +0200

    implemented ordering cards on main page

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ projectsRoutes = projectsRoutes.map(
     "/"
 );
 
+var cards = [];
 projectsRoutes.forEach(function (projectRoute, i) {
   fetch(projectRoute + "info.json")
     .then((res) => res.text())
@@ -18,12 +19,15 @@ projectsRoutes.forEach(function (projectRoute, i) {
       project = JSON.parse(text);
 
       let card = document.createElement("article");
+      cards.push(card);
 
       card.className = "card";
       card.innerHTML = `
         <img src="${project["thumbnail"]}" alt="${project["name"]}">
         <h3>${project["name"]}</h3>
+        <p class="createdDate">${project["createdDate"]}</p>
         <p>${project["description"]}</p>
+        
     `;
 
       card.style.order = i;
@@ -36,3 +40,49 @@ projectsRoutes.forEach(function (projectRoute, i) {
     })
     .catch((e) => console.error(e));
 });
+
+let orderForm = document.getElementById("order");
+addEventListener("change", function () {
+  let selected = orderForm.selectedIndex;
+  let options = orderForm.options;
+  console.log(options[selected].value);
+
+  switch (options[selected].value) {
+    case "AZ":
+      cards.sort((a, b) => {
+        return sortByChildText(a, b, 1, true);
+      });
+      break;
+    case "ZA":
+      cards.sort((a, b) => {
+      return sortByChildText(a, b, 1, false);
+      });
+      break;
+    case "newest":
+      cards.sort((a, b) => {
+        return sortByChildText(a, b, 2, false);
+      });
+      break;
+    case "oldest":
+      cards.sort((a, b) => {
+        return sortByChildText(a, b, 2, true);
+      });
+      break;
+  }
+
+  updateCardeOrder();
+});
+
+function sortByChildText(a, b, index, asc) {
+  if (asc) {
+    return a.children[index].innerText.localeCompare(b.children[index].innerText);
+  } else {
+    return b.children[index].innerText.localeCompare(a.children[index].innerText);
+  }
+}
+
+function updateCardeOrder() {
+  cards.forEach((card, i) => {
+    card.style.order = i;
+  });
+}

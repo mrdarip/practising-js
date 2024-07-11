dbb502cc30af44b6990a68554c2e92e70f179df0
mrdarip <darioelas2005@gmail.com>
2024-07-11 10:15:50 +0200

    modularized gravity effect

--- projects/TypingFX/script.js
+++ projects/TypingFX/script.js
@@ -10,27 +10,34 @@ myInput.addEventListener("input", function () {
 function createCharParticle(character) {
   let charParticle = document.createElement("div");
   charParticle.innerText = character;
-  charParticle.style.position = "absolute";
-  charParticle.style.left = (myInput.offsetLeft+myInput.offsetWidth) + "px";
-  charParticle.style.top = (myInput.offsetTop + myInput.offsetHeight) + "px";
+
+  setGravityEffect(charParticle);
+
   document.body.appendChild(charParticle);
+}
+
+function setGravityEffect(node) {
+  node.style.position = "absolute";
+  node.style.left = myInput.offsetLeft + myInput.offsetWidth + "px";
+  node.style.top = myInput.offsetTop + myInput.offsetHeight + "px";
+
   let lifeTime = 0;
   let xSpeed = 1 + Math.random() * 2;
 
   let liveLoop = setInterval(function () {
-    let x = parseInt(charParticle.style.left);
-    let y = parseInt(charParticle.style.top);
+    let x = parseInt(node.style.left);
+    let y = parseInt(node.style.top);
 
     let a = lifeTime / 100;
 
-    charParticle.style.left = x + xSpeed + "px";
-    charParticle.style.top = y + a + "px";
+    node.style.left = x + xSpeed + "px";
+    node.style.top = y + a + "px";
 
     lifeTime += 1000 / 60;
   }, 1000 / 60);
 
   setTimeout(function () {
     window.clearInterval(liveLoop);
-    charParticle.remove();
+    node.remove();
   }, 5000);
 }

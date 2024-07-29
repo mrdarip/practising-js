80a98d332809a140e5b432deae5c4638b54918d2
mrdarip <darioelas2005@gmail.com>
2024-07-29 23:40:19 +0200

    implemented setting slide and html for later full navigation implementation

--- projects/HtmlPresentation/js/presentationViewer.js
+++ projects/HtmlPresentation/js/presentationViewer.js
@@ -2,21 +2,25 @@
 var url = new URL(window.location.href);
 var presentationUrl = url.searchParams.get("presentationUrl");
 
+var presentation
 // Load the presentation json
 fetch(presentationUrl)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        presentation = data;
 
         var generalData = document.createElement("div");
         generalData.innerHTML = "<p> Style:" + JSON.stringify(data.style, null, 2); + "</p>";
         document.body.appendChild(generalData);
 
-        // Create the slides
-        data.slides.forEach(slide => {
-            var slideElement = document.createElement("div");
-            slideElement.classList.add("slide");
-            slideElement.innerHTML = "<h2>" + slide.title + "</h2>" + "<p>" + slide.content + "</p>" + "<img src='" + slide.image + "'/>";
-            document.body.appendChild(slideElement);
-        });
+        setSlide(0);
     });
+
+    function setSlide(slideNumber){
+        var slide = presentation.slides[slideNumber];
+
+        var slideElement = document.createElement("div");
+        slideElement.classList.add("slide");
+        slideElement.innerHTML = "<h2>" + slide.title + "</h2>" + "<p>" + slide.content + "</p>" + "<img src='" + slide.image + "'/>";
+        document.body.appendChild(slideElement);
+    }

f20e93d3e4d5a2ee2a840a39fd82bb620cba8682
mrdarip <darioelas2005@gmail.com>
2024-07-29 23:49:38 +0200

    implemented seeing previous and next slide

--- projects/HtmlPresentation/js/presentationViewer.js
+++ projects/HtmlPresentation/js/presentationViewer.js
@@ -4,6 +4,8 @@ var presentationUrl = url.searchParams.get("presentationUrl");
 
 var presentation
 
+var slideNumber = 0;
+
 const slideTitle = document.getElementById("slideTitle");
 const slideContent = document.getElementById("slideContent");
 const slideImage = document.getElementById("slideImage");
@@ -18,13 +20,24 @@ fetch(presentationUrl)
         generalData.innerHTML = "<p> Style:" + JSON.stringify(data.style, null, 2); + "</p>";
         document.body.appendChild(generalData);
 
-        setSlide(0);
+        setSlide(slideNumber);
     });
 
-    function setSlide(slideNumber){
-        var slide = presentation.slides[slideNumber];
-        document.title = slide.title;
-        slideTitle.innerHTML = slide.title;
-        slideContent.innerHTML = slide.content;
-        slideImage.src = slide.image;
-    }
+function setSlide(slideNumber){
+    var slide = presentation.slides[slideNumber];
+        
+    document.title = slide.title;
+    slideTitle.innerHTML = slide.title;
+    slideContent.innerHTML = slide.content;
+    slideImage.src = slide.image;
+}
+
+function nextSlide(){
+    slideNumber++;
+    setSlide(slideNumber);
+}
+
+function previousSlide(){
+    slideNumber--;
+    setSlide(slideNumber);
+}

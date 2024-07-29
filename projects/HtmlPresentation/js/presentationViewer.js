06bbb0bd0ea7edcc5bc531ef519785b0d4cef4fa
mrdarip <darioelas2005@gmail.com>
2024-07-29 23:34:35 +0200

    implemented displaying presentation details on presentation viewer

--- projects/HtmlPresentation/js/presentationViewer.js
+++ projects/HtmlPresentation/js/presentationViewer.js
@@ -7,4 +7,16 @@ fetch(presentationUrl)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+
+        var generalData = document.createElement("div");
+        generalData.innerHTML = "<p> Style:" + JSON.stringify(data.style, null, 2); + "</p>";
+        document.body.appendChild(generalData);
+
+        // Create the slides
+        data.slides.forEach(slide => {
+            var slideElement = document.createElement("div");
+            slideElement.classList.add("slide");
+            slideElement.innerHTML = "<h2>" + slide.title + "</h2>" + "<p>" + slide.content + "</p>" + "<img src='" + slide.image + "'/>";
+            document.body.appendChild(slideElement);
+        });
     });
